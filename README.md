f31bfef9add920f75377ccc4efcb3c8bda0867c3
Jack Gugel <jagu2118@colorado.edu>
2019-11-08 07:03:03 +0000

    added insert_key_value_pairs, hash functions submodule, and README

---
+++ README.md
@@ -0,0 +1,13 @@
+# binary_tree.py
+    #has code for unbalanced binary tree
+    
+# avl_tree
+    #submodule with code for avl tree
+    
+# hash_tables
+    #submodule with code for hash table
+
+# insert_key_value_pairs
+    #chooses whether to use binary tree, avl tree, or hash tables
+    
+# could not get codes to run to get benchmarking data
---
+++ insert_key_value_pairs.py
@@ -0,0 +1,19 @@
+import binary_tree
+insert avl_tree
+insert hash_functions
+import argparse
+import avl.py
+
+
+
+if __name__ == '__main__':
+    
+
+    
+     for l in open(sys.argv[1]):
+        if sys.argv[2] == 'hash':
+            print(h_ascii_sum(l, 10000))
+        elif sys.argv[2] == 'AVL':
+            print(AVLnode(l, 10000))
+        elif sys.argv[2] == 'tree':
+            print(Node(l, 10000))
